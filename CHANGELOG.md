11aff657ec7b1269085d1804529b93178da82c4f
Weston Jossey <weston.jossey@gmail.com>
2014-08-17 13:20:45 -0400

    Add a changelog and bump the version.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 4.0.1 (August 17, 2014)
+
+Bugfixes:
+  - Support the force flag when installing slugs
+  - Fix a bug with the post install script that could generate errors
--- lib/slugforge/version.rb
+++ lib/slugforge/version.rb
@@ -2,7 +2,7 @@ module Slugforge
   module Version
     MAJOR = 4
     MINOR = 0
-    PATCH = 0
+    PATCH = 1
   end
 
   VERSION = [Version::MAJOR, Version::MINOR, Version::PATCH].join('.')
